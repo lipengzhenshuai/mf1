3240b0f07cafb42915a9aa8373c9031e8bef13b5
superboy <247934357@qq.com>
2019-07-22 22:53:51 +0800

    增加mongoose

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,24 @@
 const router = require('koa-router')()
-
+const Person = require('../dbs/models/person');
 router.prefix('/users')
 
 router.get('/', function (ctx, next) {
-  ctx.body = 'this is a users response!'
+    ctx.body = 'this is a users response!'
 })
 
 router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
+    ctx.body = 'this is a users/bar response'
+})
+
+router.get('/addPerson', async function (ctx, next) {
+    const person = new Person({
+        name: 'lipeng',
+        age: '25'
+    });
+    await person.save();
+    ctx.body = {
+        code: 0
+    }
 })
 
 module.exports = router
